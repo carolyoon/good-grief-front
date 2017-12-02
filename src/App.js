373bb6a63532263b80cbf669b57a84856c47f7d5
Quynh Nguyen <qnguyenx3@gmail.com>
2017-12-02 13:24:46 -0800

    Establish foundation of routers

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+
+          <div className="navigation-bar">
+            <Link className="navigation-text" to="/">Home</Link>
+          </div>
+
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/profile" component={User} />
+            <Route render={() => <h1>Page not found</h1>} />
+          </Switch>
+
+        </div>
+      </Router>
     );
   }
 }
