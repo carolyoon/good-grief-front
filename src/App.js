babd2731308f482bdcf84af15ab9f175a73fa4ac
Quynh Nguyen <qnguyenx3@gmail.com>
2017-12-02 15:15:16 -0800

    Establish user component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
+import Home from './components/Home';
+import User from './components/User';
+import NewJournalEntryForm from './components/NewJournalEntryForm';
+
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
 
-          <div className="navigation-bar">
-            <Link className="navigation-text" to="/">Home</Link>
+          <div className='navigation-bar'>
+            <Link className='navigation-text' to='/'>Home</Link>
           </div>
 
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/profile" component={User} />
+            <Route exact path='/' component={Home} />
+            <Route exact path='/profile' component={User} />
             <Route render={() => <h1>Page not found</h1>} />
           </Switch>
 
--- src/components/User.js
+++ src/components/User.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import NewJournalEntryForm from './NewJournalEntryForm';
 
 class User extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      journal_entries: [],
+      displayNewJournalEntryForm: false
+    }
+  }
 
+  toggleJournalEntryFormState() {
+    this.setState(prevState => ({
+      displayNewJournalEntryForm: !prevState.displayNewJournalEntryForm
+    }));
+  }
 
   render() {
     return (
-      <div className="user-profile-container">
+      <div className='user-profile-container'>
         <h1>User's Profile</h1>
+
+        <div className='journal-entries-container'>
+          <NewJournalEntryForm
+            userId={this.props.match.params.id}
+            displayNewJournalEntryForm={this.state.displayNewJournalEntryForm}
+            toggleJournalEntryFormState={this.toggleJournalEntryFormState}
+          />
+
+          <ul>
+            {this.state.journal_entries.map((journal) => {
+              return (
+                <div className='journal-entries-list-container'>
+                  <li>{journal.content}</li>
+                </div>
+              )
+            })}
+          </ul>
+        </div>
+
       </div>
     );
   }
