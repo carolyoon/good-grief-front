d2444401bb207b11e8cc898559843fb5d0628def
Quynh Nguyen <qnguyenx3@gmail.com>
2017-12-02 13:33:52 -0800

    Create components folder

---
+++ src/components/User.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+
+class User extends React.Component {
+
+
+  render() {
+    return (
+      <div className="user-profile-container">
+        <h1>User's Profile</h1>
+      </div>
+    );
+  }
+}
+
+export default User;
