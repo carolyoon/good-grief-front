2d56b6064cf08644be3eabfad4e24860f7a69715
Caroline & Shannon Nolan <pair+carolyoon.shannonnolan17@devbootcamp.com>
2017-12-03 14:58:40 -0800

    Implement advice posts Write method for axios call to get advice posts, make advice posts component, and display on denial page

---
+++ src/components/Denial.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import AdvicePost from './AdvicePost';
+import img from '../denial_images/blockYourEx.png';
+import axios from 'axios';
+
+class Denial extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      advicePosts : []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/advice_posts')
+    .then(res => {
+      const advicePosts = res.data.map ( (post) =>
+        ({id: post.id, content: post.content}))
+      this.setState( {advicePosts})
+    })
+
+  }
+  render() {
+    return(
+      <div className="denial-container">
+      <span className="stage-name"><h1>The DENIAL Stage</h1></span>
+        <div className="helpful-apps">
+          <h3>Helpful Apps</h3>
+            <ul>
+              <li>
+              <img className="denial-image" src={require("../denial_images/blockYourEx.png")} />
+              <p>Block Your Ex: a Chrome and Firefox-based plugin that allows you to remove an ex’s Twitter, Facebook and blog from your view in one go.</p>
+              </li><br />
+              <li>
+              <img className="denial-image" src={require("../denial_images/killSwitch.png")} />
+              <p>Killswitch: a mobile app that removes all traces of your ex from your Facebook by deleting pictures, videos, posts and status updates that tagged both of you</p>
+              </li><br />
+              <li>
+              <img className="denial-image" src={require("../denial_images/massPasswordReset.png")} />
+              <p>Mass Password Reset: a Firefox extension that allows you to change the password for numerous shared accounts at once.</p>
+              </li><br />
+            </ul>
+        </div>
+        <div className="helpful-articles">
+          <h3>Helpful Articles</h3>
+          <ul>
+            <li><a href="https://www.psychologytoday.com/blog/me-we/201501/the-9-stages-grieving-breakup-no-2-denial">All About Denial</a></li><br />
+            <li><a href="https://pairedlife.com/breakups/Feelings-of-Denial-When-a-Relationship-Ends">Feelings of Denial When a Relationship Ends</a></li><br />
+            <li><a href="https://datingtips.match.com/over-denial-breakup-42642.html">How to Get Over Denial About a Breakup</a></li><br />
+          </ul>
+        </div>
+        <div className='advice-posts'>
+          <h3>Helpful Advice</h3>
+          <ul>
+            {this.state.advicePosts.map (key =>
+              <AdvicePost id={key.id} content={key.content} />
+            )}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Denial;
--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,24 +1,12 @@
 import React from 'react';
+import Denial from './Denial';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
 class Stage extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      advicePosts : []
-    }
-  }
-
   render() {
     return(
       <div className='stage-container'>
-        <h1>Stage Name</h1>
-
-        <div className='advice-posts-container'>
-          <ul>
-
-          </ul>
-        </div>
+        <Denial  />
       </div>
     )
   }
