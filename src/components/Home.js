b4c58ed122fc591a775ec30511918f0e6659f6f5
AllyJo <gatorally245@gmail.com>
2017-12-03 14:11:56 -0800

    realign text

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ class Home extends React.Component {
           <h4>Disclaimer:</h4>
           <p>Good Grief DOES NOT provide medical advice.
             The information on this site is not intended or implied to be a substitute for professional medical advice, diagnosis, or treatment. All content, including text, graphics, images, and information, contained on or available through this website is for general information purposes only. Good Grief makes no representation and assumes no responsibility for the accuracy of the information contained on or available through this website. You are encouraged to confirm any information obtained from or through this website with other sources, and to consult a medical professional, if desired.
-              NEVER DISREGARD PROFESSIONAL MEDICAL ADVICE OR DELAY SEEKING MEDICAL TREATMENT BECAUSE OF SOMETHING YOU HAVE READ OR ACCESSED THROUGH THIS WEBSITE.
+              <strong>NEVER DISREGARD PROFESSIONAL MEDICAL ADVICE OR DELAY SEEKING MEDICAL TREATMENT BECAUSE OF SOMETHING YOU HAVE READ OR ACCESSED THROUGH THIS WEBSITE.</strong>
               Our purpose is to promote self awareness and assistance in self-healing. Always seek the advice of a medical professional if you have questions about your mental health or wellbeing. </p>
         </div>
       </div>
