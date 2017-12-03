9e290d557d7888ecd4c5a9a80928ab6806fcad8b
Quynh Nguyen <qnguyenx3@gmail.com>
2017-12-02 20:38:55 -0800

    WIP filter goals and journal entries

--- src/components/User.js
+++ src/components/User.js
@@ -12,15 +12,63 @@ class User extends React.Component {
         'Journal Entries'
       ],
       journal_entries: [],
+      goals: [],
       displayNewJournalEntryForm: false
     };
 
+    this.goalsCall = this.goalsCall.bind(this);
+    this.journalEntriesCall = this.journalEntriesCall.bind(this);
     this.updateOption = this.updateOption.bind(this);
     this.toggleJournalEntryFormState = this.toggleJournalEntryFormState.bind(this);
   }
 
+  goalsCall() {
+     axios.get('http://localhost:3001/goals')
+    .then(function(response) {
+      const goals = []
+      response.data.map(goal => goals.push(goal))
+      this.setState({ goals })
+    })
+  }
+
+  journalEntriesCall() {
+    axios.get('http://localhost:3001/journal_entries')
+    .then(function(response) {
+      const journal_entries = []
+      response.data.map(journal_entry => journal_entries.push(journal_entry))
+      this.setState({ journal_entries })
+    })
+  }
+
+  componentDidMount() {
+    this.goalsCall();
+    this.journalEntriesCall();
+  }
+
   updateOption(option) {
     this.setState({ selectedOption: option })
+    if(this.state.selectedOption === 'goals'){
+      return (
+        <div className='goals-view-list-container'>
+          <ul>
+            {this.state['goals'].map((goal) =>
+              <li className='list-item'>{goal}</li>
+            )}
+          </ul>
+        </div>
+      )
+    }
+    if(this.state.selectedOption === 'journal_entries'){
+      return (
+        <div className='journal-entries-view-list-container'>
+          <ul>
+            {this.state['journal_entries'].map((journal_entry) =>
+              <li className='list-item'>{journal_entry}</li>
+            )}
+          </ul>
+        </div>
+      )
+    }
   }
 
   toggleJournalEntryFormState() {
