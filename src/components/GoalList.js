2e96c6e761a5692c1ad1b5604654ad915868b11a
Quynh Nguyen <qnguyenx3@gmail.com>
2017-12-03 14:32:24 -0800

    Add goal and journal entry list components

---
+++ src/components/GoalList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class GoalList extends React.Component {
+
+  render() {
+    return(
+
+    );
+  }
+}
+
+export default GoalList;
---
+++ src/components/JournalEntryList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class JournalEntryList extends React.Component {
+
+  render() {
+    return(
+
+    );
+  }
+}
+
+export default JournalEntryList;
