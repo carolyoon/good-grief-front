fb21f5bf2c108505f3ea224fa9c44b6ac1713bca
Quynh Nguyen <qnguyenx3@gmail.com>
2017-12-04 17:22:46 -0800

    WIP add styling to goals and journal entries view

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -4,8 +4,8 @@ class GoalList extends React.Component {
 
   render() {
       return(
-        <ul>
-          <li className='goals-list-container' onClick={this.props.deleteCompletedGoal}>
+        <ul className='goals-list-container'>
+          <li onClick={this.props.deleteCompletedGoal}>
             <input type='checkbox' checked={this.props.goalCompleted} onChange={this.props.deleteCompletedGoal} />
             {this.props.goal.content}
           </li>
