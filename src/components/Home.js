1b2f7a8fff23c2a051aa3fdafb2c3cd571cf206c
Quynh Nguyen <qnguyenx3@gmail.com>
2017-12-06 17:24:14 -0800

    Complete styling of home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,15 +6,50 @@ class Home extends React.Component {
   render() {
     return (
       <div className="home-container">
-        <h1 className='mission-statement-header'>Mission Statement:</h1>
-        <p>We strive to create a safe environment for those with a broken heart from a relationship dissolution, and want to start the healing process. We aim provide resources, concerned with the five common stages of grief, to those that want to know how others have overcome the grieving process.</p>
+        <div className="mission-statement">
+          <h1 className='mission-statement-header'>Mission Statement:</h1>
+          <p className='mission-statement-body'>We strive to create a safe environment for those with a broken heart from a relationship dissolution and want to begin the healing process. As such, we aim to provide resources, concerned with the five common stages of grief, to those who want to know how others have overcome the grieving process.</p>
+        </div>
 
         <div className='understanding-grief-container'>
-          <img className='5-stages-of-grief-image' src={require('../5-stages-of-grief.png')} />
-          <div className='image-and-title'>
-            <FontAwesome.FaAngleDoubleDown className='fa-angle-double-down' />
-            <span className='understanding'>Understanding</span>
-            <p className='the-stages-of-grief'>the Five Stages of Grief</p>
+          <div className='stages-of-grief-image'>
+            <img height='450' src={require('../5-stages-of-grief.png')} />
+          </div>
+
+          <div>
+            <div className='understanding-grief-title'>
+              <span className='understanding'>Understanding</span>
+              <p className='the-stages-of-grief'>the Five Stages of Grief</p>
+            </div>
+
+            <div className='understanding-grief-body'>
+              <dl>
+                <dt>𝟙 Denial</dt>
+                <dd className='grief-explanation'><span className='heart-bullet'>❧</span> Protects you from overwhelming emotions</dd>
+                <dd className='grief-explanation'><span className='heart-bullet'>❧</span> May feel like a numbness or isolation from reality</dd>
+                <dd className='grief-explanation'><span className='heart-bullet'>❧</span> Should not be confused with a lack of caring</dd>
+
+                <dt>𝟚 Anger</dt>
+                <dd className='grief-explanation'><span className='heart-bullet'>❧</span> Anger may be directed at the people or situations associated with the break-up</dd>
+                <dd className='grief-explanation'><span className='heart-bullet'>❧</span> May feel like it is a great idea to tell anyone and everyone what a psycho-crazy vixen your ex was </dd>
+                <dd className='grief-explanation'><span className='heart-bullet'>❧</span> Take your time in this stage and understand your options</dd>
+
+                <dt>𝟛 Bargaining</dt>
+                <dd className='grief-explanation'><span className='heart-bullet'>❧</span> Can occur before or after the break-up</dd>
+                <dd className='grief-explanation'><span className='heart-bullet'>❧</span> Involves thinking about possibilities to make the relationship work through negotiations, threats, and/or magic </dd>
+                <dd className='grief-explanation'><span className='heart-bullet'>❧</span> Unresolved issues in this stage can lead to intense remorse or guilt</dd>
+
+                <dt>𝟜 Depression</dt>
+                <dd className='grief-explanation'><span className='heart-bullet'>❧</span> You may experience deep grief and sadness beyond what you imagined</dd>
+                <dd className='grief-explanation'><span className='heart-bullet'>❧</span> This stage can feel like it will last forever or like there is no point in going on</dd>
+                <dd className='grief-explanation'><span className='heart-bullet'>❧</span> This stage is natural and not something that you should "snap out of"</dd>
+
+                <dt>𝟝 Acceptance</dt>
+                <dd className='grief-explanation'><span className='heart-bullet'>❧</span> Acceptance entails making peace with the loss, letting go of the relationship, and slowly moving forward with your life</dd>
+                <dd className='grief-explanation'><span className='heart-bullet'>❧</span> Does not mean that you are all right with the reality, just that you have accepted it</dd>
+                <dd className='grief-explanation'><span className='heart-bullet'>❧</span> Happens a little bit at a time, as grief is a long process</dd>
+              </dl>
+            </div>
           </div>
         </div>
 
