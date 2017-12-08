fc1041107213cf1ec0deb188649608f50d0e07fd
Quynh Nguyen <qnguyenx3@gmail.com>
2017-12-08 11:17:04 -0800

    Fix stylig

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -22,44 +22,44 @@ class Tracker extends React.Component {
     if (this.props.stageId === 2) {
       return (
         <header>
-          <a href='/denial' onClick={this.handle} >Denial</a>
-          <a href='/anger' onClick={this.handle} style={{background: '#DF744A', color: '#FFFFFF'}} >Anger</a>
-          <a href='/bargaining' onClick={this.handle} >Bargaining</a>
-          <a href='/depression' onClick={this.handle} >Depression</a>
-          <a href='/acceptance' onClick={this.handle} >Acceptance</a>
+          <a className='stages-tracker-buttons' href='/denial' onClick={this.handle} >Denial</a>
+          <a className='stages-tracker-buttons' href='/anger' onClick={this.handle} style={{background: '#DF744A', color: '#FFFFFF'}} >Anger</a>
+          <a className='stages-tracker-buttons' href='/bargaining' onClick={this.handle} >Bargaining</a>
+          <a className='stages-tracker-buttons' href='/depression' onClick={this.handle} >Depression</a>
+          <a className='stages-tracker-buttons' href='/acceptance' onClick={this.handle} >Acceptance</a>
         </header>
       )
     }
     if (this.props.stageId === 3) {
       return (
         <header>
-          <a href='/denial' onClick={this.handle} >Denial</a>
-          <a href='/anger' onClick={this.handle} >Anger</a>
-          <a href='/bargaining' onClick={this.handle} style={{background: '#DF744A', color: '#FFFFFF'}} >Bargaining</a>
-          <a href='/depression' onClick={this.handle} >Depression</a>
-          <a href='/acceptance' onClick={this.handle} >Acceptance</a>
+          <a className='stages-tracker-buttons' href='/denial' onClick={this.handle} >Denial</a>
+          <a className='stages-tracker-buttons' href='/anger' onClick={this.handle} >Anger</a>
+          <a className='stages-tracker-buttons' href='/bargaining' onClick={this.handle} style={{background: '#DF744A', color: '#FFFFFF'}} >Bargaining</a>
+          <a className='stages-tracker-buttons' href='/depression' onClick={this.handle} >Depression</a>
+          <a className='stages-tracker-buttons' href='/acceptance' onClick={this.handle} >Acceptance</a>
         </header>
       )
     }
     if (this.props.stageId === 4) {
       return (
         <header>
-          <a href='/denial' onClick={this.handle} >Denial</a>
-          <a href='/anger' onClick={this.handle} >Anger</a>
-          <a href='/bargaining' onClick={this.handle} >Bargaining</a>
-          <a href='/depression' onClick={this.handle} style={{background: '#DF744A', color: '#FFFFFF'}} >Depression</a>
-          <a href='/acceptance' onClick={this.handle} >Acceptance</a>
+          <a className='stages-tracker-buttons' href='/denial' onClick={this.handle} >Denial</a>
+          <a className='stages-tracker-buttons' href='/anger' onClick={this.handle} >Anger</a>
+          <a className='stages-tracker-buttons' href='/bargaining' onClick={this.handle} >Bargaining</a>
+          <a className='stages-tracker-buttons' href='/depression' onClick={this.handle} style={{background: '#DF744A', color: '#FFFFFF'}} >Depression</a>
+          <a className='stages-tracker-buttons' href='/acceptance' onClick={this.handle} >Acceptance</a>
         </header>
       )
     }
     if (this.props.stageId === 5) {
       return (
         <header>
-          <a href='/denial' onClick={this.handle} >Denial</a>
-          <a href='/anger' onClick={this.handle} >Anger</a>
-          <a href='/bargaining' onClick={this.handle} >Bargaining</a>
-          <a href='/depression' onClick={this.handle} >Depression</a>
-          <a href='/acceptance' onClick={this.handle} style={{background: '#DF744A', color: '#FFFFFF'}} >Acceptance</a>
+          <a className='stages-tracker-buttons' href='/denial' onClick={this.handle} >Denial</a>
+          <a className='stages-tracker-buttons' href='/anger' onClick={this.handle} >Anger</a>
+          <a className='stages-tracker-buttons' href='/bargaining' onClick={this.handle} >Bargaining</a>
+          <a className='stages-tracker-buttons' href='/depression' onClick={this.handle} >Depression</a>
+          <a className='stages-tracker-buttons' href='/acceptance' onClick={this.handle} style={{background: '#DF744A', color: '#FFFFFF'}} >Acceptance</a>
         </header>
       )
     }
